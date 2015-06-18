37f97ee87b76ad2e7371ecdace5c3c7112e5b8b6
liorg <lior.grosman@gmail.com>
2015-06-18 11:39:01 +0300

    add excel

---
+++ MechanismDocMailMerge/Excel/SpreedsheetHelper.cs
@@ -0,0 +1,199 @@
+﻿using DocumentFormat.OpenXml.Packaging;
+using DocumentFormat.OpenXml.Spreadsheet;
+using System;
+using System.Collections.Generic;
+
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Qualibase.Uniliver.Schedulers.ImportExcel.Helpers
+{
+    public class SpreedsheetHelper
+    {
+        public static System.Data.DataTable ReadAsDataTable(string fileName)
+        {
+            System.Data.DataTable data = new System.Data.DataTable();
+            //  DataTable dataTable = new DataTable();
+            using (SpreadsheetDocument spreadSheetDocument = SpreadsheetDocument.Open(fileName, false))
+            {
+                WorkbookPart workbookPart = spreadSheetDocument.WorkbookPart;
+                IEnumerable<Sheet> sheets = spreadSheetDocument.WorkbookPart.Workbook.GetFirstChild<Sheets>().Elements<Sheet>();
+                string relationshipId = sheets.First().Id.Value;
+                WorksheetPart worksheetPart = (WorksheetPart)spreadSheetDocument.WorkbookPart.GetPartById(relationshipId);
+                Worksheet workSheet = worksheetPart.Worksheet;
+                SheetData sheetData = workSheet.GetFirstChild<SheetData>();
+                IEnumerable<Row> rows = sheetData.Descendants<Row>();
+
+                foreach (Cell cell in rows.ElementAt(0))
+                {
+                    data.Columns.Add(GetCellValue(spreadSheetDocument, cell));
+                }
+
+                foreach (Row row in rows)
+                {
+                    System.Data.DataRow dataRow = data.NewRow();
+                    IEnumerable<Cell> cells = SpreedsheetHelper.GetRowCells(row);
+                    int i = 0;
+                    foreach (Cell cell in cells)
+
+                    //  for (int i = 0; i < row.Descendants<Cell>().Count(); i++)
+                    //{dataRow[i] = GetCellValue(spreadSheetDocument, row.Descendants<Cell>().ElementAt(i)); }
+                    {
+                        if (i > data.Columns.Count)
+                            break;
+                        dataRow[i] = GetCellValue(spreadSheetDocument, cell);
+                        i++;
+                    }
+
+                    data.Rows.Add(dataRow);
+                }
+
+            }
+            data.Rows.RemoveAt(0);
+
+            return data;
+        }
+
+        static string GetCellValue(SpreadsheetDocument document, Cell cell)
+        {
+            SharedStringTablePart stringTablePart = document.WorkbookPart.SharedStringTablePart;
+            if (cell == null || cell.CellValue == null)
+                return "";
+            string value = cell.CellValue.InnerXml;
+
+            if (cell.DataType != null && cell.DataType.Value == CellValues.SharedString)
+            {
+                return stringTablePart.SharedStringTable.ChildElements[Int32.Parse(value)].InnerText;
+            }
+            else
+            {
+                return value;
+            }
+        }
+       
+        public static void ExportDSToExcel(System.Data.DataSet ds, string destination)
+        {
+            using (var workbook = SpreadsheetDocument.Create(destination, DocumentFormat.OpenXml.SpreadsheetDocumentType.Workbook))
+            {
+                var workbookPart = workbook.AddWorkbookPart();
+                workbook.WorkbookPart.Workbook = new DocumentFormat.OpenXml.Spreadsheet.Workbook();
+                workbook.WorkbookPart.Workbook.Sheets = new DocumentFormat.OpenXml.Spreadsheet.Sheets();
+
+                uint sheetId = 1;
+
+                foreach (System.Data.DataTable table in ds.Tables)
+                {
+                    var sheetPart = workbook.WorkbookPart.AddNewPart<WorksheetPart>();
+                    var sheetData = new DocumentFormat.OpenXml.Spreadsheet.SheetData();
+                    sheetPart.Worksheet = new DocumentFormat.OpenXml.Spreadsheet.Worksheet(sheetData);
+
+                    DocumentFormat.OpenXml.Spreadsheet.Sheets sheets = workbook.WorkbookPart.Workbook.GetFirstChild<DocumentFormat.OpenXml.Spreadsheet.Sheets>();
+                    string relationshipId = workbook.WorkbookPart.GetIdOfPart(sheetPart);
+
+                    if (sheets.Elements<DocumentFormat.OpenXml.Spreadsheet.Sheet>().Count() > 0)
+                    {
+                        sheetId =
+                            sheets.Elements<DocumentFormat.OpenXml.Spreadsheet.Sheet>().Select(s => s.SheetId.Value).Max() + 1;
+                    }
+
+                    DocumentFormat.OpenXml.Spreadsheet.Sheet sheet = new DocumentFormat.OpenXml.Spreadsheet.Sheet() { Id = relationshipId, SheetId = sheetId, Name = table.TableName };
+                    sheets.Append(sheet);
+
+                    DocumentFormat.OpenXml.Spreadsheet.Row headerRow = new DocumentFormat.OpenXml.Spreadsheet.Row();
+
+                    List<String> columns = new List<string>();
+                    foreach (System.Data.DataColumn column in table.Columns)
+                    {
+                        columns.Add(column.ColumnName);
+
+                        DocumentFormat.OpenXml.Spreadsheet.Cell cell = new DocumentFormat.OpenXml.Spreadsheet.Cell();
+                        cell.DataType = DocumentFormat.OpenXml.Spreadsheet.CellValues.String;
+                        cell.CellValue = new DocumentFormat.OpenXml.Spreadsheet.CellValue(column.ColumnName);
+                        headerRow.AppendChild(cell);
+                    }
+
+                    sheetData.AppendChild(headerRow);
+
+                    foreach (System.Data.DataRow dsrow in table.Rows)
+                    {
+                        DocumentFormat.OpenXml.Spreadsheet.Row newRow = new DocumentFormat.OpenXml.Spreadsheet.Row();
+                        foreach (String col in columns)
+                        {
+                            DocumentFormat.OpenXml.Spreadsheet.Cell cell = new DocumentFormat.OpenXml.Spreadsheet.Cell();
+                            cell.DataType = DocumentFormat.OpenXml.Spreadsheet.CellValues.String;
+                            cell.CellValue = new DocumentFormat.OpenXml.Spreadsheet.CellValue(dsrow[col].ToString()); //
+                            newRow.AppendChild(cell);
+                        }
+
+                        sheetData.AppendChild(newRow);
+                    }
+                }
+            }
+        }
+        ///<summary>returns an empty cell when a blank cell is encountered
+        ///</summary>
+        public static IEnumerable<Cell> GetRowCells(Row row)
+        {
+            int currentCount = 0;
+
+            foreach (DocumentFormat.OpenXml.Spreadsheet.Cell cell in
+                row.Descendants<DocumentFormat.OpenXml.Spreadsheet.Cell>())
+            {
+                string columnName = GetColumnName(cell.CellReference);
+
+                int currentColumnIndex = ConvertColumnNameToNumber(columnName);
+
+                for (; currentCount < currentColumnIndex; currentCount++)
+                {
+                    yield return new DocumentFormat.OpenXml.Spreadsheet.Cell();
+                }
+
+                yield return cell;
+                currentCount++;
+            }
+        }
+
+        /// <summary>
+        /// Given a cell name, parses the specified cell to get the column name.
+        /// </summary>
+        /// <param name="cellReference">Address of the cell (ie. B2)</param>
+        /// <returns>Column Name (ie. B)</returns>
+        public static string GetColumnName(string cellReference)
+        {
+            // Match the column name portion of the cell name.
+            var regex = new System.Text.RegularExpressions.Regex("[A-Za-z]+");
+            var match = regex.Match(cellReference);
+
+            return match.Value;
+        }
+
+        /// <summary>
+        /// Given just the column name (no row index),
+        /// it will return the zero based column index.
+        /// </summary>
+        /// <param name="columnName">Column Name (ie. A or AB)</param>
+        /// <returns>Zero based index if the conversion was successful</returns>
+        /// <exception cref="ArgumentException">thrown if the given string
+        /// contains characters other than uppercase letters</exception>
+        public static int ConvertColumnNameToNumber(string columnName)
+        {
+            var alpha = new System.Text.RegularExpressions.Regex("^[A-Z]+$");
+            if (!alpha.IsMatch(columnName)) throw new ArgumentException();
+
+            char[] colLetters = columnName.ToCharArray();
+            Array.Reverse(colLetters);
+
+            int convertedValue = 0;
+            for (int i = 0; i < colLetters.Length; i++)
+            {
+                char letter = colLetters[i];
+                int current = i == 0 ? letter - 65 : letter - 64; // ASCII 'A' = 65
+                convertedValue += current * (int)Math.Pow(26, i);
+            }
+
+            return convertedValue;
+        }
+    }
+
+}
